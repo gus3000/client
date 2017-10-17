b8215c91f23cabe85415130d7e67fb0f1b634d9c
Augustin Chateau <gus3000spam@gmail.com>
2017-10-17 15:33:49 +0200

    added color in buttons

--- src/sidebar/components/dropdown-menu-btn.js
+++ src/sidebar/components/dropdown-menu-btn.js
@@ -2,24 +2,26 @@
 
 // @ngInject
 function DropdownMenuBtnController($timeout) {
-  var self = this;
-  this.toggleDropdown = function($event) {
-    $event.stopPropagation();
-    $timeout(function () {
-      self.onToggleDropdown();
-    }, 0);
-  };
+    var self = this;
+    this.toggleDropdown = function($event) {
+	$event.stopPropagation();
+	$timeout(function () {
+	    self.onToggleDropdown();
+	}, 0);
+    };
+    //this.blah = "coucou";
 }
 
 module.exports = {
-  controller: DropdownMenuBtnController,
-  controllerAs: 'vm',
-  bindings: {
-    isDisabled: '<',
-    label: '<',
-    dropdownMenuLabel: '@',
-    onClick: '&',
-    onToggleDropdown: '&',
-  },
-  template: require('../templates/dropdown-menu-btn.html'),
+    controller: DropdownMenuBtnController,
+    controllerAs: 'vm',
+    bindings: {
+	isDisabled: '<',
+	label: '<',
+	addedStyle: '@',
+	dropdownMenuLabel: '@',
+	onClick: '&',
+	onToggleDropdown: '&',
+    },
+    template: require('../templates/dropdown-menu-btn.html'),
 };
